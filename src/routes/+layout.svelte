35869c8367fc344ec74c10e442bfea13e3685cd3
Saksham Saini <sakshamsaini18@gmail.com>
2024-11-25 14:34:31 -0500

    add debug logs

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,9 @@
 	import '../app.css';
 	import { goto } from '$app/navigation';
 	import { user } from '$lib/store';
+	console.log('layout');
 	user.subscribe(($user) => {
+		console.log($user);
 		if (!$user) {
 			goto('/auth');
 		}
