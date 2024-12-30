8c6f072e38cdd0ae08b971ae26c3c124e2ab2b8d
Saksham Saini <sakshamsaini18@gmail.com>
2024-12-29 21:31:14 -0500

    update build command

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,19 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			options: {
+				headers: {
+					'Cache-Control': 'max-age=0, no-cache, no-store, must-revalidate',
+					'Content-Security-Policy':
+						"default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'",
+					'X-Frame-Options': 'DENY',
+					'X-Content-Type-Options': 'nosniff',
+					Pragma: 'no-cache',
+					Expires: '0'
+				}
+			}
+		})
 	}
 };
 
