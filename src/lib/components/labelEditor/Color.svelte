40a946ff4f728147b40ca7af6d336e1ffa00a7f0
Saksham Saini <sakshamsaini18@gmail.com>
2025-02-01 22:41:48 -0500

    fix color

--- src/lib/components/labelEditor/Color.svelte
+++ src/lib/components/labelEditor/Color.svelte
@@ -6,6 +6,6 @@
 	class="flex w-full flex-row gap-[10px] rounded-md px-[6px] py-[4px] hover:bg-primary-dark-gray"
 	{onclick}
 >
-	<div class="h-[25px] w-[25px] rounded-md" style="background-color: {color}"></div>
+	<div class="h-[25px] w-[25px] rounded-md" style:background-color={color}></div>
 	{@render children?.()}
 </button>
